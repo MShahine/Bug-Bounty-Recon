c27133ca1f7600763039139068c6f2e94ed327f5
MShahine <64935281+MShahine@users.noreply.github.com>
2020-08-31 20:01:37 +0100

    Create domain-check.sh

---
+++ domain-check.sh
@@ -0,0 +1,1 @@
+chaos -d domain -o dm -silent | httpx -silent | xargs -P100 -I@ gospider -c 30 -t 15 -d 4 -a -H "x-forwarded-for: 127.0.0.1" -H "User-Agent: Mozilla/5.0 (Linux; U; Android 2.2) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1" -s @ 
